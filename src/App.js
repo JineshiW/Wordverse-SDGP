3dff217455b3d6d29ab58f7d1b5952217e363879
Sanith Anusara <sanithanusara@Saniths-MacBook-Air.local>
2024-01-31 23:14:14 +0530

    adding file

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="Box1">
+      <div className="Box2">
+        <div className='ProfilePic'>
+        </div>
+        <h3 className='centered-h3'>User Name</h3>
+        <div className='Box7'>
+          <div className='Box8'></div>
+          <div className='Box9'></div>
+          <div className='Box10'></div>
+          <div className='Box11'></div>
+          <div className='Box12'></div>
+          <div className='Box13'></div>
+        </div>
+      </div>
+      <div className='Box3'>
+        <h3>Bio...</h3>
+      </div>
+      <div className='Box4'>
+        <h3>Level...</h3>
+      </div>
+      <div className='Box5'>
+        <h3>Goals...</h3>
+      </div>
+      <div className='Box6'>
+        <h3>Language skills...</h3>
+      </div>
     </div>
+    
   );
 }
 
