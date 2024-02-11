efc4aeb27b48b6eb167e35c5d8af3a28641bb209
Jineshi <jineshiw@gmail.com>
2024-02-11 19:42:09 +0530

    modified Quiz.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Main.css';
 
 export default function Main() {
+
+    const inputRef= useRef(null)
+
   return (
-    <div>Main</div>
+    <div className='container'>
+        <h1 className='title text-light'>Quiz Game</h1>
+
+        <ol>
+            <li>You will be asked 10 questions one after another</li>
+            <li>10 points is awarded for the currect answer.</li>
+            <li>Each question hads three options. Uou can choose only one option.</li>
+            <li>You van review and change answers before the quiz ends.</li>
+            <li>The result will be declared at the end of the quiz.</li>
+        </ol>
+
+        <form id='form'>
+            <input ref={inputRef} className='userid' type='text' placeholder='Username*'/>
+        </form>
+
+        <div className='start'>
+            {/* directing routing path to quiz though Link component */}
+            <Link className='btn' to={'quiz'}> Start Quiz</Link>
+        </div>
+
+    </div>
   )
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
 export default function Quiz() {
+
+  function onNext(){
+    console.log('On next click')
+  }
+  function onPrev(){
+    console.log('On prev click')
+  }
+
   return (
-    <div>Quiz</div>
+    <div className='container'>
+      <h1 className='title text-light'>Quiz Game</h1>
+
+      {/* display questions */}
+
+      {/* adding buttons for next and previous */}
+      <div className='grid'>
+        <button className='btn prev' onClick={onPrev}>Previous</button>
+        <button className='btn next' onClick={onNext}>Next</button>
+      </div>
+    </div>
   )
 }
