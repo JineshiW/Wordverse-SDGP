c82d92af166bfc18263e0c6529bdf8d1c58c9aba
Jineshi <jineshiw@gmail.com>
2024-02-14 13:35:50 +0530

    fixed bug in radio buttons of question page by modifying Questions.js file

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,29 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function Questions() {
+    const [checked, setChecked] = useState(undefined);
 
-    const [checked, setChecked] = useState(undefined)
-
-    function onSelect(){
-        setChecked(true)
-        console.log('radio button change')
+    function onSelect() {
+        console.log('radio button change');
     }
 
-  return (
-    <div className='questions'>
-        <h2 className='text-light'>Simple Question 1</h2>
+    return (
+        <div className='questions'>
+            <h2 className='text-light'>Simple Question 1</h2>
 
-        <u>
-            <li>
-                <input
-                 type='radio'
-                 value={checked} 
-                 name='options'
-                 id='qi-option'
-                 onChange={onSelect}
-                 />
-            </li>
-        </u>
-    </div>
-  )
+            <ul> {/* Use ul instead of u */}
+                <li>
+                    <input
+                        type='radio'
+                        value={false}
+                        name='options'
+                        id='q1-option'
+                        onChange={onSelect}  
+                    />
+                    <label className='text-primary' htmlFor="q1-option">option</label>
+                    <div className='check checked'></div>
+                </li>
+            </ul> 
+        </div>
+    );
 }
