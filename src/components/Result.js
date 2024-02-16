254c575f9bd43ed40eef9b95f304743432f9a675
Jineshi <jineshiw@gmail.com>
2024-02-16 17:48:25 +0530

    modified the code in Result.js file by adding restart button and and quiz results section

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,15 +1,47 @@
 import React from 'react'
+import '../styles/Result.css'
+import {Link} from 'react-router-dom';
 
 export default function Result() {
+
+  function onRestart(){
+    console.log("on Restart")
+  }
+
   return (
     <div className='container'>
       <h1 className='title text-light'>Quiz Game</h1>
 
       <div className='result flex-center'>
-        <div className='flez'>
+        <div className='flex'>
           <span>Username</span>
-          <span className='bold'>Lalalalala</span>
+          <span className='bold'>Hannah</span>
         </div>
+        <div className='flex'>
+          <span>Total Quiz Points: </span>
+          <span className='bold'>50</span>
+        </div>
+        <div className='flex'>
+          <span>Total Questions Answered: </span>
+          <span className='bold'>05</span>
+        </div>
+        <div className='flex'>
+          <span>Total Attempts: </span>
+          <span className='bold'>03</span>
+        </div>
+        <div className='flex'>
+          <span>Total Earned Points: </span>
+          <span className='bold'>50</span>
+        </div>
+        <div className='flex'>
+          <span>Quiz Result: </span>
+          <span className='bold'>Passed</span>
+        </div>
+      </div>
+      
+      <div className="start">
+        {/* navigate user to restart the quiz */}
+        <Link className='btn' to={'/'} onClick={onRestart}>Restart</Link>
       </div>
     </div>
   )
