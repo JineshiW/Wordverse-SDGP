4201a9150e06009df968322a5e0fdc71282c3a1c
Jineshi <jineshiw@gmail.com>
2024-02-16 21:48:04 +0530

    created resultReducer.js file

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,15 @@ import { combineReducers, configureStore} from '@reduxjs/toolkit';
 
 // call reducers
 import { questionReducer } from './questionReducer';
+import { resultReducer } from './resultReducer';
 
-// creating reducer functions 
+// combining reducers with combineReducers function 
 const rootReducer = combineReducers({
     // initializing questions property with questionReducer
-    questions: questionReducer
+    questions: questionReducer,
+    // initializing result property with resultReducer
+    result: resultReducer
 })
+
+// create store with reducer
+export default configureStore({ reducer : rootReducer})
