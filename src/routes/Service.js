87d6680de2431474b012b86047f1fbe5289856dc
AbishananGaneshalingam <abishanan@hotmail.com>
2024-02-18 22:00:33 +0530

    first comit

---
+++ src/routes/Service.js
@@ -0,0 +1,12 @@
+import Navbar from "../Components/Navbar"
+
+function Service (){
+    return(
+        <>
+        <Navbar />
+         <h1> This is Service</h1>
+        </>
+    )
+}
+
+export default Service;
