58a7688c47fd084cb4e7e70fe65bbb5682873d94
Jineshi <jineshiw@gmail.com>
2024-02-20 00:29:45 +0530

    modified Questions.js and fetchQuestions.js files to render the questions and options in array form to console

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import data from '../database/data';
+import { useSelector } from 'react-redux';
+
+// importing custom hook
+import { useFetchQuestion } from '../hooks/fetchQuestion';
 
 export default function Questions() {
 
     const [checked, setChecked] = useState(undefined);
-
+    const [{isLoading, apiData, serverError}] = useFetchQuestion()
     const question= data[0]
+
+    const questions = useSelector(state => state.questions.queue[0])
 
     // creating useEffect react Hook
     useEffect(() =>{
-        console.log(question)
+        // get the initial value of the queue
+        console.log(questions)
     })
-
-    
+        
     // creating onSelect fuction
     function onSelect() {
         // console.log('radio button change');
     }
 
+    if(isLoading) return<h3 className='text-light'>isLoading</h3>
+    if(serverError) return<h3 className='text-light'>{serverError || "Unknown Error"}</h3>
+    
     return (
         <div className='questions'>
             <h2 className='text-light'>{question.question}</h2>
--- src/hooks/fetchQuestion.js
+++ src/hooks/fetchQuestion.js
@@ -1,18 +1,37 @@
 import { useEffect, useState } from "react"
 import data from "../database/data"
+import { useDispatch } from "react-redux"
+
+// import redux actions 
+import * as Action from '../redux/questionReducer'
 
 // fetch question hook to fetch api data and set value to store
 export const useFetchQuestion =()=>{
+    // creating useDispatch hook function
+    const dispatch =useDispatch();
     const [getData, setGetData] = useState({isLoading: false, apiData: [], serverError: null})
 
     // Creating useEffect hook function
-    useEffect(()=> {
-        // update useState
-        setGetData(prev => ({...prev, isLoading: true}))
+    useEffect(() => {
+        const fetchData = async () => {
+            setGetData(prev => ({ ...prev, isLoading: true }));
+            try {
+                let question = await data;
+                if (question.length > 0) {
+                    setGetData(prev => ({ ...prev, apiData: question, isLoading: false }));
+                    dispatch(Action.startExamAction(question));
+                } else {
+                    throw new Error("No Question Available");
+                }
+            } catch (error) {
+                setGetData(prev => ({ ...prev, isLoading: false, serverError: error }));
+            }
+        };
+    
+        fetchData();
+    
+    }, [dispatch]);
+    
 
-        // async function to fetch the backend data
-        (async ()=>{
-            
-        })
-    })
+    return [getData, setGetData];
 }
