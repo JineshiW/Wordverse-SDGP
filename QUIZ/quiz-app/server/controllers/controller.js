62f09b059ac8e1ec63a1ef1e0a589e8bc39ec345
JineshiW <jineshiw@gmail.com>
2024-03-04 18:36:59 +0530

    created data.js file to store questions and answers , modified controller.js by updating routing api for results get, delet, and post requests, updated resultSchema.js file

--- QUIZ/quiz-app/server/controllers/controller.js
+++ QUIZ/quiz-app/server/controllers/controller.js
@@ -1,31 +1,68 @@
-
+import Questions from "../models/questionSchema.js";
+import Results from "../models/resultSchema.js";
+import questions, {answers} from "../database/data.js";
 
 // get all questions
 export async function getQuestions(req, res){
-    res.json("questions api get request");
+    try {
+        const q = await Questions.find()
+        res.json(q)
+    } catch (error) {
+        res.json({error})
+    }
 }
 
 // insert all questions
 export async function insertQuestions(req, res){
-    res.json("questions api post request")
+    try {
+        Questions.insertMany({questions, answers}, function(error,data){
+            res.json({msg: "Data Saved Successfully...!"})
+        })
+    } catch (error) {
+        res.json({error})
+    }
 }
 
 // delete all questions
 export async function dropQuestions(req, res){
-    res.json("questions api delete request");
+    try {
+        Questions.deleteMany();
+        res.json({msg: "Data Deleted Successfully...!"})
+    } catch (error) {
+        res.json({error})
+    }
 }
 
 // get all results
 export async function getResult(req, res){
-    res.json("result api get request");
+    try {
+        const r =await Results.find()
+        res.json(r)
+    } catch (error) {
+        res.json({error})
+    }
 }
 
 // post all results
 export async function storeResult(req, res){
-    res.json("result api post request");
+    try {
+        const {username, result, attempts, points, achived} = req.body;
+        if(!username && !result) throw new Error("Data Not Provided...!");
+
+        Results.create({username, result, attempts, points, achived},function(error,data){
+            res.json({msg: "Result Saved Successfully...!"})
+        })
+    } catch (error) {
+        res.json({error})
+    }
 }
 // delete all results
 export async function dropResult(req, res){
-    res.json("result api delete request");
+    try {  
+        await Results.deleteMany();
+        res.json({msg: "Result Deleted Successfully...!"})
+    } catch (error) {
+        res.json({error})
+    }
 }
 
---
+++ QUIZ/quiz-app/server/database/data.js
@@ -0,0 +1,50 @@
+// creating all the questions and the relevent options for the quiz
+export default [
+    {
+        id: 1,
+        question : "How many letters are there in the English Alphebet ?",
+        options : [
+            '25',
+            '26',
+            '27',
+        ]
+    },
+    {
+        id: 2,
+        question : "Which word correctly completes the sentence? 'I ____ a book yesterday'",
+        options : [
+            'read',
+            'red',
+            'rid',
+        ]
+    },
+    {
+        id: 3,
+        question : "What does the word 'apple' refer to?",
+        options : [
+            'A color',
+            'A shape',
+            'A type of fruit',
+        ]
+    },
+    {
+        id: 4,
+        question : "Which sentence is correct?",
+        options : [
+            'She is go to the store.',
+            'She goes to the store.',
+            'She going to the store.',
+        ]
+    },
+    {
+        id: 5,
+        question : "Which word is a pronoun?",
+        options : [
+            'They',
+            'Happy',
+            'Run',
+        ]
+    }
+]
+
+export const answers =[1,0,2,1,0];
