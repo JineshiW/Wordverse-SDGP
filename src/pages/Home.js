0585d144b21d5f10c76557c4014df4a360459677
Sanith Anusara <148668755+SanithAnusara@users.noreply.github.com>
2024-03-12 18:19:33 +0530

    Sanith updates-03-12
    Updated version of Client

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import Destination from "../Components/Destination";
+import Footer from "../Components/Footer";
+import Hero from "../Components/Hero"
+import Navbar from "../Components/Navbar"
+import Course from "../Components/Course";
+import HomeImg from "../assets/Home.jpg"
+
+function Home (){
+    return(
+        <>
+        <Navbar/>
+        <Hero
+            cName = "hero"
+            heroImg ={HomeImg}
+            title = "Which Language do you want to learn"
+            text = "Level up your Language skills through the continuous learning."
+            buttonText ="View Courses"
+            url = "/"
+            btnClass = "show"
+        />
+        <Destination />
+        <Course />
+        <Footer />
+        </>
+    )
+}
+
+export default Home;
