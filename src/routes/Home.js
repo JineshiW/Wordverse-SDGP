f9eeee53603fbdbd007535766544fc84ac6649b1
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-03-19 20:07:10 +0530

    Updated home .js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,28 +1,40 @@
-import Destination from "../Components/Destination";
-import Footer from "../Components/Footer";
-import Hero from "../Components/Hero"
-import Navbar from "../Components/Navbar"
-import Course from "../Components/Course";
-import HomeImg from "../assets/Home.jpg"
+// Importing necessary components and assets
+import Destination from "../Components/Destination"; // Importing the Destination component from "../Components/Destination.js"
+import Footer from "../Components/Footer"; // Importing the Footer component from "../Components/Footer.js"
+import Hero from "../Components/Hero"; // Importing the Hero component from "../Components/Hero.js"
+import Navbar from "../Components/Navbar"; // Importing the Navbar component from "../Components/Navbar.js"
+import Course from "../Components/Course"; // Importing the Course component from "../Components/Course.js"
+import HomeImg from "../assets/Home.jpg"; // Importing the image asset for the Home page
 
+// Defining the Home functional component
 function Home (){
     return(
         <>
+        {/* Rendering the Navbar component */}
         <Navbar/>
+
+        {/* Rendering the Hero component */}
         <Hero
-            cName = "hero"
-            heroImg ={HomeImg}
-            title = "Which Language do you want to learn"
-            text = "Level up your Language skills through the continuous learning."
-            buttonText ="View Courses"
-            url = "/"
-            btnClass = "show"
+            cName = "hero" // Custom class name for styling
+            heroImg ={HomeImg} // Setting the background image for the hero section
+            title = "Which Language do you want to learn" // Title for the hero section
+            text = "Level up your Language skills through continuous learning." // Text content for the hero section
+            buttonText ="View Courses" // Text for the button displayed in the hero section
+            url = "/" // URL to navigate when the button is clicked (in this case, navigating to the home page)
+            btnClass = "show" // CSS class to show the button
         />
+
+        {/* Rendering the Destination component */}
         <Destination />
+
+        {/* Rendering the Course component */}
         <Course />
+
+        {/* Rendering the Footer component */}
         <Footer />
         </>
     )
 }
 
+// Exporting the Home component as the default export
 export default Home;
