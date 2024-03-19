4f7fed23682844d5b836fc2f13b5aab3272e40bc
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-03-19 20:08:15 +0530

    Update Service.js

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -1,25 +1,33 @@
-import Hero from "../Components/Hero";
-import Navbar from "../Components/Navbar";
-import AboutImg from "../assets/course.jpg";
-import Footer from "../Components/Footer";
-import Course from "../Components/Course";
+// Importing necessary components and assets
+import Hero from "../Components/Hero"; // Importing the Hero component from "../Components/Hero.js"
+import Navbar from "../Components/Navbar"; // Importing the Navbar component from "../Components/Navbar.js"
+import AboutImg from "../assets/course.jpg"; // Importing the image asset for the Course page
+import Footer from "../Components/Footer"; // Importing the Footer component from "../Components/Footer.js"
+import Course from "../Components/Course"; // Importing the Course component from "../Components/Course.js"
 
+// Defining the CoursePage functional component
 function CoursePage() {
     return (
         <>
+            {/* Rendering the Navbar component */}
             <Navbar />
+
+            {/* Rendering the Hero component */}
             <Hero
-                cName="hero-mid"
-                heroImg={AboutImg}
-                title="Language Learning"
-
-
-                btnClass="hide"
+                cName="hero-mid" // Custom class name for styling
+                heroImg={AboutImg} // Setting the background image for the hero section
+                title="Language Learning" // Title for the hero section
+                btnClass="hide" // CSS class to hide a button, if any
             />
+
+            {/* Rendering the Course component */}
             <Course />
+
+            {/* Rendering the Footer component */}
             <Footer />
         </>
     )
 }
 
+// Exporting the CoursePage component as the default export
 export default CoursePage;
