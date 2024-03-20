d12e39f60e00836b52c12d6d31a35dace7933bf0
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-03-20 23:45:49 +0530

    authmiddeleware  changes

--- SDGP_server/middleware/authMiddleware.js
+++ SDGP_server/middleware/authMiddleware.js
@@ -1,36 +1,50 @@
-const jwt = require('jsonwebtoken')
-const asyncHandler = require('express-async-handler')
-const User = require('../auth/userModel')
+// Import required modules and packages
+const jwt = require('jsonwebtoken'); // JSON Web Token (JWT) library for Node.js
+const asyncHandler = require('express-async-handler'); // Middleware to handle asynchronous functions
+const User = require('../auth/userModel'); // Import the User model for database operations
 
+// Define a middleware function named 'protect' to authenticate and authorize users
 const protect = asyncHandler(async (req, res, next) => {
-  let token
+  let token; // Variable to store the JWT extracted from the request header
 
+  // Check if the Authorization header is present and starts with 'Bearer'
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      // Get token from header
-      token = req.headers.authorization.split(' ')[1]
+      // Extract the token from the Authorization header (format: 'Bearer <token>')
+      token = req.headers.authorization.split(' ')[1];
 
-      // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      // Verify the authenticity of the token using the JWT_SECRET stored in the environment variables
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Get user from the token
-      req.user = await User.findById(decoded.id).select('-password')
+      // Find the user associated with the decoded user ID from the token
+      // Exclude the user's password from the returned document
+      req.user = await User.findById(decoded.id).select('-password');
 
-      next()
+      next(); // Call the next middleware function in the chain
     } catch (error) {
-      console.log(error)
-      res.status(401)
-      throw new Error('Not authorized')
+      // If an error occurs during token verification or user retrieval, log the error
+      console.log(error);
+
+      // Set the response status code to 401 (Unauthorized)
+      res.status(401);
+
+      // Throw an error to be caught by the error handling middleware
+      throw new Error('Not authorized');
     }
   }
 
+  // If no token is found in the Authorization header
   if (!token) {
-    res.status(401)
-    throw new Error('Not authorized, no token')
-  }
-})
+    // Set the response status code to 401 (Unauthorized)
+    res.status(401);
 
-module.exports = { protect }
+    // Throw an error to be caught by the error handling middleware
+    throw new Error('Not authorized, no token');
+  }
+});
+
+// Export the 'protect' middleware function to make it available for use in other parts of the application
+module.exports = { protect };
