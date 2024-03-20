9d6b8ee37d432d04855c36bc34f49470fd8dcaff
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-03-21 00:47:39 +0530

    testing work

--- SDGP_Admin/src/App.test.js
+++ SDGP_Admin/src/App.test.js
@@ -1,8 +1,15 @@
+// Import necessary functions and components from the testing-library/react library
 import { render, screen } from '@testing-library/react';
+// Import the main component of the application to be tested
 import App from './App';
 
+// Define a test case using the test function provided by Jest
 test('renders learn react link', () => {
+  // Render the main component of the application
   render(<App />);
+  // Use screen.getByText() to find an element with the text 'learn react' (case insensitive)
   const linkElement = screen.getByText(/learn react/i);
+  // Use Jest's expect() function to make assertions about the element
+  // Check if the 'learn react' link is present in the rendered component
   expect(linkElement).toBeInTheDocument();
 });
