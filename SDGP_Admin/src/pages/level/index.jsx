f81af08bbfedbd586a7ea582f115ae3eb2f79389
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-03-21 01:00:51 +0530

    Update index.jsx

--- SDGP_Admin/src/pages/level/index.jsx
+++ SDGP_Admin/src/pages/level/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import LevelDataTable from "../../components/layouts/tables/level/level";
 
+// Functional component representing the Level Page
 const LevelPage = () => {
   return (
     <div>
-      <br />
+      <br /> {/* Adds a line break */}
+      {/* Renders the LevelDataTable component */}
       <LevelDataTable />
     </div>
   );
 };
 
-export default LevelPage;
+export default LevelPage; // Exports the LevelPage component
