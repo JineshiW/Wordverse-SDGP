727bf1025970a707811b1b97eda5fa837c8b805e
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-03-21 01:03:03 +0530

    Update Dashboard.jsx

--- SDGP_Admin/src/pages/Dashboard.jsx
+++ SDGP_Admin/src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
+// Functional component representing the Dashboard page
 const AllApps = () => {
+  // Renders an <h1> element displaying the text "Dashboard"
   return <h1>Dashboard</h1>;
 };
 
-export default AllApps;
+export default AllApps; // Exports the AllApps component
