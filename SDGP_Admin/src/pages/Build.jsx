fa740d108113a6c523785883859ed93d978576a6
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-03-21 01:02:23 +0530

    Update Build.jsx

--- SDGP_Admin/src/pages/Build.jsx
+++ SDGP_Admin/src/pages/Build.jsx
@@ -1,8 +1,12 @@
 import { useParams } from "react-router-dom";
 
+// Functional component representing the Build page
 const Build = () => {
+  // Extracts the parameter 'bID' from the URL using useParams hook
   const { bID } = useParams();
+  
+  // Renders an <h1> element displaying the text "Build /" followed by the value of 'bID'
   return <h1>Build / {bID}</h1>;
 };
 
-export default Build;
+export default Build; // Exports the Build component
