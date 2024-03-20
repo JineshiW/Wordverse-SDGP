2973b080fd561a94a3068a1bcaec2bc2f5bdc22f
Abishanan Ganeshalingam <redishberry5@gmail.com>
2024-03-20 08:47:39 +0530

    Updates in level route.js

--- SDGP_server/level/level.routes.js
+++ SDGP_server/level/level.routes.js
@@ -1,5 +1,8 @@
+// Import the Express framework
 const express = require('express')
+// Create a router object using Express Router
 const router = express.Router()
+// Import controller functions from 'level.controller' file
 const {
     getSingle,
     getAll,
@@ -9,26 +12,26 @@ const {
     filterWithCourseId
 } = require('./level.controller')
 
-
+// Define routes
 
 //Get all 
-router.get('/',getAll) 
+router.get('/',getAll)  // Route to get all documents
 
-router.get('/course/:courseID',filterWithCourseId)
+router.get('/course/:courseID',filterWithCourseId)// Route to get documents filtered by course ID
 
-//Get a single document
+// Route to get a single document by its ID
 router.get('/:id',getSingle)
 
 //Get a by unit id
 
 //POST a new document
-router.post('/', createNew)
+router.post('/', createNew) // Route to create a new document
 
 //Delete a  document
-router.delete('/:id', deleteSinle)
+router.delete('/:id', deleteSinle) // Route to delete a document by its ID
 
 //Update a document
-router.patch('/:id',updateDocument)
+router.patch('/:id',updateDocument) // Route to update a document by its ID
 
-
+// Export the router object to be used in the application
 module.exports = router;
