980a2d70e8f749552088f36540eb382fc0007e73
sahan <sahan.20220255@iit.ac.lk>
2024-03-22 08:16:36 +0530

    adding explaining to the chatbot/server.js

--- ChatBot/server.js
+++ ChatBot/server.js
@@ -1,20 +1,23 @@
 // node --version # Should be >= 18
 // npm install @google/generative-ai express
 
-const express = require('express');
+const express = require('express'); // Import the express library for building web servers
 const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config() // Load environment variables from .env file
 
-const app = express();
-const port = process.env.PORT || 3002;
-app.use(express.json());
-const MODEL_NAME = "gemini-1.0-pro";
-const API_KEY = process.env.API_KEY;
+const app = express(); // Create an express application
+const port = process.env.PORT || 3002; 
+app.use(express.json()); // Middleware to parse incoming request bodies as JSON
 
-async function runChat(userInput) {
-  const genAI = new GoogleGenerativeAI(API_KEY);
-  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+const MODEL_NAME = "gemini-1.0-pro"; // Define the name of the generative model
+const API_KEY = process.env.API_KEY; // Load the API key from environment variables
 
+// Function to interact with the generative model and obtain a response based on user input
+async function runChat(userInput) { 
+  const genAI = new GoogleGenerativeAI(API_KEY); // Create an instance of the GoogleGenerativeAI class
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME }); // Get the specific generative model
+
+  // Configuration for generating responses
   const generationConfig = {
     temperature: 0.9,
     topK: 1,
@@ -22,6 +25,7 @@ async function runChat(userInput) {
     maxOutputTokens: 2048,
   };
 
+  // Safety settings to block harmful content
   const safetySettings = [
     {
       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
@@ -41,23 +45,29 @@ async function runChat(userInput) {
     },
   ];
 
+  // Start a chat session with the model using specified settings and user input
   const chat = model.startChat({
     generationConfig,
     safetySettings,
     history: [],
   });
 
+   // Send user input to the chat session and await the response
   const result = await chat.sendMessage(userInput);
   const response = result.response;
   return response.text();
 }
 
+// Serve the index.html file when root URL is accessed
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+// Serve the loader.gif file
 app.get('/loader.gif', (req, res) => {
   res.sendFile(__dirname + '/loader.gif');
 });
+
+// Handle POST requests to /chat endpoint
 app.post('/chat', async (req, res) => {
   try {
     const userInput = req.body?.userInput;
